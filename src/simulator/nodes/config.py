a6382f4284d0d5d6640922c2134a5cc42a8adf86
averak <abe12@mccc.jp>
2022-12-11 17:01:32 +0900

    [feat] タイムスタンプごとの座標を記録

---
+++ src/simulator/nodes/config.py
@@ -0,0 +1,23 @@
+class Config:
+    MEASURED_RESULT_FILENAME: str = "data.jsonl"
+    """
+    測定結果を保存するファイル名(~/.rosに保存される)
+    """
+
+    TURTLEBOT3_MODEL_NAME: str = "turtlebot3_waffle"
+    """
+    TurtleBot3のモデル名
+    """
+
+    PERIOD_TO_MOVE: float = 2.0
+    """
+    ロボットを前進させる期間 [秒]
+    """
+
+    MAX_VELOCITY: float = 0.26
+    """
+    最大速度
+
+    waffle: 0.26
+    burger: 0.22
+    """
