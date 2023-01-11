9557fb2ccda4bd879cd26aaf5d6adae1ee00add8
averak <abe12@mccc.jp>
2023-01-12 05:50:32 +0900

    [feat] keras.backendを使わない

--- src/training/nnet/base_nnet.py
+++ src/training/nnet/base_nnet.py
@@ -18,7 +18,8 @@ class BaseNNet(metaclass=ABCMeta):
     """
 
     def __init__(self):
-        # tf.config.run_functions_eagerly(True)
+        # TODO: lossのデバッグが完了したら削除する
+        tf.config.run_functions_eagerly(True)
         self.build_model()
         # self.model.summary()
 
@@ -69,13 +70,13 @@ class BaseNNet(metaclass=ABCMeta):
         """
 
         # 分母が極端に小さくなることを防ぐためのオフセット
-        epsilon = K.constant(K.epsilon())
+        epsilon = tf.constant(K.epsilon())
 
         # y = [xの移動量, yの移動量] ^ T
-        y = K.expand_dims(y_true[:, 0:2])
+        y = tf.expand_dims(y_true[:, 0:2], axis=-1)
 
         # ŷ = [推定したxの移動量, 推定したyの移動量] ^ T
-        ŷ = K.expand_dims(y_pred[:, 0:2])
+        ŷ = tf.expand_dims(y_pred[:, 0:2], axis=-1)
 
         # Σ = U * Λ * U^T のように分解する（Λは対角行列、Uは回転行列）
         # Λ = [[λ1, 0], [0, λ2]]
@@ -86,21 +87,24 @@ class BaseNNet(metaclass=ABCMeta):
         # 下記より、u1からUを求められる（NNはu1のみ出力する）
         #   1. 1列目と2列目はそれぞれ単位ベクトル（=u2が一意に定まる）
         #   2. 1列目と2列目は直交する
-        u1 = K.expand_dims(y_pred[:, 4])
-        u2 = K.expand_dims(y_pred[:, 5])
+        u1 = tf.expand_dims(y_pred[:, 4], axis=-1)
+        u2 = tf.expand_dims(y_pred[:, 5], axis=-1)
 
-        u1_u2_vector_length = K.sqrt(u1 ** 2 + u2 ** 2)
+        u1_u2_vector_length = tf.sqrt(u1 ** 2 + u2 ** 2)
         u1 /= u1_u2_vector_length
         u2 /= u1_u2_vector_length
-        U = K.concatenate([K.expand_dims(K.concatenate([u1, u2])), K.expand_dims(K.concatenate([-u2, u1]))])
+        U = tf.concat([
+            tf.expand_dims(tf.concat([u1, u2], axis=-1), axis=-1),
+            tf.expand_dims(tf.concat([-u2, u1], axis=-1), axis=-1)
+        ], axis=-1)
 
         # Σ = U * Λ * U^T
         # Σ^-1 = U * Λ^1 * U^T
         Σ_inv = tf.matmul(tf.matmul(U, Λ_inv), tf.linalg.matrix_transpose(U))
         det_Σ = 1 / (Σ_inv[:, 0, 0] * Σ_inv[:, 1, 1] - Σ_inv[:, 0, 1] * Σ_inv[:, 1, 0])
 
-        return K.mean(
-            K.log((2 * np.pi) ** 2 * det_Σ) + \
+        return tf.reduce_mean(
+            tf.math.log((2 * np.pi) ** 2 * det_Σ) + \
             tf.matmul(tf.matmul(tf.linalg.matrix_transpose(y - ŷ), Σ_inv), (y - ŷ))
         )
 
@@ -112,12 +116,12 @@ class BaseNNet(metaclass=ABCMeta):
 
         ŷ1 = y_pred[:, 0]
         ŷ2 = y_pred[:, 1]
-        ξ1 = K.relu(y_pred[:, 2])
-        ξ2 = K.relu(y_pred[:, 3])
+        ξ1 = tf.nn.relu(y_pred[:, 2])
+        ξ2 = tf.nn.relu(y_pred[:, 3])
         u1 = y_pred[:, 4]
         u2 = y_pred[:, 5]
 
-        return K.stack([ŷ1, ŷ2, ξ1, ξ2, u1, u2], 1)
+        return tf.stack([ŷ1, ŷ2, ξ1, ξ2, u1, u2], axis=1)
 
     @staticmethod
     def x_movement_amount_metric(y_true: Tensor, y_pred: Tensor) -> Tensor:
