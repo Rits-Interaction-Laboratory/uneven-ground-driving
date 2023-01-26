24c3c7f269faedcd54fd454c9db69dce4f28034b
averak <abe12@mccc.jp>
2023-01-26 09:36:18 +0900

    [feat] 共分散行列に対応する楕円を描画

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow.python.keras.backend as K
 from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
+from matplotlib.patches import Ellipse
 
 from src.training.driving_record import DrivingRecord, DrivingRecordRepository
 from src.training.nnet.base_nnet import BaseNNet
@@ -74,7 +75,7 @@ def output_train_history(history):
     plt.figure()
     plt.plot(history.history['x_mae'])
     plt.plot(history.history['val_x_mae'])
-    plt.ylabel('|y_1 - ŷ_1|')
+    plt.ylabel('|x_1 - x̂_1|')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'])
     plt.savefig("./analysis/history_x_mae.png")
@@ -82,7 +83,7 @@ def output_train_history(history):
     plt.figure()
     plt.plot(history.history['y_mae'])
     plt.plot(history.history['val_y_mae'])
-    plt.ylabel('|y_2 - ŷ_2|')
+    plt.ylabel('|x_2 - x̂_2|')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'])
     plt.savefig("./analysis/history_y_mae.png")
@@ -90,7 +91,7 @@ def output_train_history(history):
     # plt.figure()
     # plt.plot(history.history['σ_x_mae'])
     # plt.plot(history.history['val_σ_x_mae'])
-    # plt.ylabel('||y_1 - ŷ_1| - σ_y1|')
+    # plt.ylabel('||x_1 - x̂_1| - σ_y1|')
     # plt.xlabel('epoch')
     # plt.legend(['train', 'val'])
     # plt.savefig("./analysis/history_σ_x_mae.png")
@@ -98,15 +99,21 @@ def output_train_history(history):
     # plt.figure()
     # plt.plot(history.history['σ_y_mae'])
     # plt.plot(history.history['val_σ_y_mae'])
-    # plt.ylabel('||y_2 - ŷ_2| - σ_y2|')
+    # plt.ylabel('||x_2 - x̂_2| - σ_y2|')
     # plt.xlabel('epoch')
     # plt.legend(['train', 'val'])
     # plt.savefig("./analysis/history_σ_y_mae.png")
 
 
+def eigsorted(cov):
+    vals, vecs = np.linalg.eigh(cov)
+    order = vals.argsort()[::-1]
+    return vals[order], vecs[:, order]
+
+
 def output_predict_results(predict_results: np.ndarray, true_movement_amounts: np.ndarray, label: str,
                            heatmap_y1_range: tuple, heatmap_y2_range: tuple, heatmap_σ1_range: tuple,
-                           heatmap_σ2_range: tuple):
+                           heatmap_σ2_range: tuple, y1_error_range: tuple, y2_error_range: tuple):
     """
     推定結果を出力
     """
@@ -121,8 +128,8 @@ def output_predict_results(predict_results: np.ndarray, true_movement_amounts: n
     plt.figure()
     plt.hist2d(ŷ1_movement_amounts, y1_movement_amounts, bins=100, cmin=1)
     plt.colorbar()
-    plt.xlabel("ŷ_1")
-    plt.ylabel("y_1")
+    plt.xlabel("x̂_1")
+    plt.ylabel("x_1")
     plt.xlim(*heatmap_y1_range)
     plt.ylim(0.0, 0.9)
     plt.savefig(f"./analysis/{label}_heatmap_x.png")
@@ -130,8 +137,8 @@ def output_predict_results(predict_results: np.ndarray, true_movement_amounts: n
     plt.figure()
     plt.hist2d(ŷ2_movement_amounts, y2_movement_amounts, bins=100, cmin=1)
     plt.colorbar()
-    plt.xlabel("ŷ_2")
-    plt.ylabel("y_2")
+    plt.xlabel("x̂_2")
+    plt.ylabel("x_2")
     plt.xlim(*heatmap_y2_range)
     plt.ylim(-0.4, 0.4)
     plt.savefig(f"./analysis/{label}_heatmap_y.png")
@@ -153,9 +160,30 @@ def output_predict_results(predict_results: np.ndarray, true_movement_amounts: n
         ax.set_ylim(-0.18, 0.18)
         plt.savefig(f"./analysis/{label}_y_ŷ_lines_expansion_{size}.png")
 
-    Σ = nnet.get_Σ(predict_results)
-    σ1_list = K.sqrt(Σ[:, 0, 0])
-    σ2_list = K.sqrt(Σ[:, 1, 1])
+    Σ_list = nnet.get_Σ(predict_results)
+
+    # 推定値を中心とする共分散行列に対応する楕円を描画
+    nstd = 2
+    for size in range(10, 301, 50):
+        fig = plt.figure()
+        ax = fig.add_subplot()
+
+        for i in range(len(predict_results[:size])):
+            vals, vecs = eigsorted(Σ_list[i])
+            theta = np.degrees(np.arctan2(*vecs[:, 0][::-1]))
+            w, h = 2 * nstd * np.sqrt(vals)
+            ell = Ellipse(xy=(ŷ1_movement_amounts[i], ŷ2_movement_amounts[i]),
+                          width=w, height=h,
+                          angle=theta, color='black')
+            ell.set_facecolor('none')
+            ax.add_artist(ell)
+
+        ax.set_xlim(-2.0, 2.0)
+        ax.set_ylim(-2.0, 2.0)
+        plt.savefig(f"./analysis/{label}_Σ_{size}.png")
+
+    σ1_list = K.sqrt(Σ_list[:, 0, 0])
+    σ2_list = K.sqrt(Σ_list[:, 1, 1])
 
     y1_errors = []
     y2_errors = []
@@ -167,14 +195,14 @@ def output_predict_results(predict_results: np.ndarray, true_movement_amounts: n
     plt.hist2d(σ1_list, y1_errors, bins=100, cmin=1)
     plt.colorbar()
     plt.xlabel("σ_1")
-    plt.ylabel("|ŷ_1 - y_1|")
+    plt.ylabel("|x̂_1 - x_1|")
     plt.savefig(f"./analysis/{label}_heatmap_σ_x_error.png")
 
     plt.figure()
     plt.hist2d(σ2_list, y2_errors, bins=100, cmin=1)
     plt.colorbar()
     plt.xlabel("σ_2")
-    plt.ylabel("|ŷ_2 - y_2|")
+    plt.ylabel("|x̂_2 - x_2|")
     plt.savefig(f"./analysis/{label}_heatmap_σ_y_error.png")
 
     # σと残差標準偏差をプロットする（ただし、残差標準偏差を求める際は平均で引かない）
@@ -216,16 +244,18 @@ def output_predict_results(predict_results: np.ndarray, true_movement_amounts: n
     plt.hist2d(σ1_standard_deviation_graph_x_list, σ1_standard_deviation_graph_y_list, bins=σ_bins, cmin=1)
     plt.colorbar()
     plt.xlabel("σ_1")
-    plt.ylabel("|ŷ_1 - y_1| standard deviation")
+    plt.ylabel("|x̂_1 - x_1| standard deviation")
     plt.xlim(*heatmap_σ1_range)
+    plt.ylim(*y1_error_range)
     plt.savefig(f"./analysis/{label}_heatmap_σ_x_standard_deviation.png")
 
     plt.figure()
     plt.hist2d(σ2_standard_deviation_graph_x_list, σ2_standard_deviation_graph_y_list, bins=σ_bins, cmin=1)
     plt.colorbar()
     plt.xlabel("σ_2")
-    plt.ylabel("|ŷ_2 - y_2| standard deviation")
+    plt.ylabel("|x̂_2 - x_2| standard deviation")
     plt.xlim(*heatmap_σ2_range)
+    plt.ylim(*y2_error_range)
     plt.savefig(f"./analysis/{label}_heatmap_σ_y_standard_deviation.png")
 
 
@@ -265,6 +295,14 @@ train_σ1_list = [K.sqrt(Σ[0, 0]).numpy() for Σ in nnet.get_Σ(train_predict_r
 train_σ2_list = [K.sqrt(Σ[1, 1]).numpy() for Σ in nnet.get_Σ(train_predict_results)]
 test_σ1_list = [K.sqrt(Σ[0, 0]).numpy() for Σ in nnet.get_Σ(test_predict_results)]
 test_σ2_list = [K.sqrt(Σ[1, 1]).numpy() for Σ in nnet.get_Σ(test_predict_results)]
+train_y1_error_list = [abs(train_predict_results[i][0] - train_true_movement_amounts[i][0]) for i in
+                       range(len(train_predict_results))]
+train_y2_error_list = [abs(train_predict_results[i][1] - train_true_movement_amounts[i][1]) for i in
+                       range(len(train_predict_results))]
+test_y1_error_list = [abs(test_predict_results[i][0] - test_true_movement_amounts[i][0]) for i in
+                      range(len(test_predict_results))]
+test_y2_error_list = [abs(test_predict_results[i][1] - test_true_movement_amounts[i][1]) for i in
+                      range(len(test_predict_results))]
 
 pred_heatmap_y1_range = (min([train_predict_results[:, 0].min(), test_predict_results[:, 0].min()]),
                          max([train_predict_results[:, 0].max(), test_predict_results[:, 0].max()]))
@@ -274,8 +312,14 @@ pred_heatmap_σ1_range = (min([min(train_σ1_list), min(test_σ1_list)]),
                          max([max(train_σ1_list), max(test_σ1_list)]))
 pred_heatmap_σ2_range = (min([min(train_σ2_list), min(test_σ2_list)]),
                          max([max(train_σ2_list), max(test_σ2_list)]))
+y1_error_range = (min([min(train_y1_error_list), min(test_y1_error_list)]),
+                  max([max(train_y1_error_list), max(test_y1_error_list)]))
+y2_error_range = (min([min(train_y2_error_list), min(test_y2_error_list)]),
+                  max([max(train_y2_error_list), max(test_y2_error_list)]))
 
 output_predict_results(train_predict_results, train_true_movement_amounts, "train", pred_heatmap_y1_range,
-                       pred_heatmap_y2_range, pred_heatmap_σ1_range, pred_heatmap_σ2_range)
+                       pred_heatmap_y2_range, pred_heatmap_σ1_range, pred_heatmap_σ2_range,
+                       y1_error_range, y2_error_range)
 output_predict_results(test_predict_results, test_true_movement_amounts, "test", pred_heatmap_y1_range,
-                       pred_heatmap_y2_range, pred_heatmap_σ1_range, pred_heatmap_σ2_range)
+                       pred_heatmap_y2_range, pred_heatmap_σ1_range, pred_heatmap_σ2_range,
+                       y1_error_range, y2_error_range)
